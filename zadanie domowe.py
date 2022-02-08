210bd731ba0056ffaff51716b819ae3bc5e20bd2
Mata120 <szczurek.marta94@gmail.com>
2022-02-08 19:14:58 +0100

    proba

---
+++ zadanie domowe.py
@@ -0,0 +1,69 @@
+# #cw 1
+#
+# str1="Datatypes"
+# sr_str1=int(len(str1)/2)
+# str3=str1[sr_str1-1:sr_str1+2]
+# print(str3)
+#
+# str1="Fullstack"
+# sr_str1=int(len(str1)/2)
+# str3=str1[sr_str1-1:sr_str1+2]
+# print(str3)
+
+# #cw2
+#
+# s1 = "FullStack"
+# s2 = "Python"
+# sr_1=int(len(s1)/2)
+# s3=s1[:sr_1]+s2+s1[sr_1:]
+# print(s3)
+#
+# cw3
+# s1 = "America"
+# s2 = "Japan"
+# sr_1 = int(len(s1)/ 2)
+# sr_2 = int(len(s2)/ 2)
+# s3 = s1[0]+s2[0]+s1[sr_1]+s2[sr_2]+s1[-1]+s2[-1]
+# print(s3)
+
+# # cw 4
+# str1 = "Python"
+# print(str1[::-1])
+
+# cw5
+# tuple1 = (10, 20, 30, 40, 50)
+# tuple1 = tuple1[::-1]
+# print(tuple1)
+
+# cw 6
+# tuple1 = ("Pomarańczowy", [10, 20, 30], (5, 15, 25))
+# print(tuple1[1][1])
+
+# cw7
+
+# lista=["Anna Kos", "Marianna Migacz", "Julia Nosak", "Adam Kwaśny", "Filip Bober", "Alex James"]
+# print(sorted(lista))
+# print(len(lista))
+
+# # cw8
+# # wersja 1
+# keys = ['Dziesieć', 'Dwadzieścia', 'Trzydzieści']
+# values = [10, 20, 30]
+# slownik={keys[0]:values[0],keys[1]:values[1],keys[2]:values[2]}
+# print(slownik)
+# wersja 2
+# keys = ['Dziesieć', 'Dwadzieścia', 'Trzydzieści']
+# values = [10, 20, 30]
+# res_dict = dict(zip(keys, values))
+# print(res_dict)
+# cw 9
+# sample_set = {"Żółty", "Pomarańczowy", "Czarny"}
+# sample_list = ["Niebieski", "Zielony", "Czerwony"]
+# sample_set.update(sample_list)
+# print(sorted(sample_set,reverse=True))
+
+# keys = ['Dziesieć', 'Dwadzieścia', 'Trzydzieści']
+# values = [10, 20, 30]
+# slownik=dict(zip(keys,values))
+# print(slownik)
+
